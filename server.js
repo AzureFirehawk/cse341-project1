6bcf3b9530cedf964c30573c966399f3a57085f3
AzureFirehawk <madison.thomas094@gmail.com>
2025-11-04 12:03:37 -0600

    Database connection and content

--- server.js
+++ server.js
@@ -1,10 +1,25 @@
 const express = require('express');
-const app = express();
+const bodyParser = require('body-parser');
+const MongoClient = require('mongodb');
+const mongodb = require('./db/connect');
 
+const app = express();
 const port = process.env.PORT || 3000;
 
-app.use('/', require('./routes'));
+app
+    .use('/', require('./routes'))
+    .use(bodyParser.json())
+    .use((req, res, next) => {
+        res.setHeader('Access-Control-Allow-Origin', '*');
+        next();
+    });
 
-app.listen(port, () => {
-    console.log(`Server running on port ${port}`);
+mongodb.initDb((err, db) => {
+    if (err) {
+        console.log(err);
+    } else {
+        app.listen(port, () => {
+            console.log('Connected to DB and listening on port ' +  port);
+        });
+    }
 });
