409fb36a38db546084e2b4df518b03acd28efe45
AzureFirehawk <madison.thomas094@gmail.com>
2025-11-04 11:40:41 -0600

    init routes

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const app = express();
 
 const port = process.env.PORT || 3000;
 
+app.use('/', require('./routes'));
+
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
 });
