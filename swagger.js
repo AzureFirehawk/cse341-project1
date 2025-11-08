f7236bcaf23a68f4f0b87a8f68053b3e9c5a44e8
AzureFirehawk <madison.thomas094@gmail.com>
2025-11-08 13:35:19 -0600

    updated swagger host

--- swagger.js
+++ swagger.js
@@ -5,7 +5,7 @@ const doc = {
     title: 'My API',
     description: 'Description'
   },
-  host: 'localhost:3000'
+  host: 'https://cse341-project1-rccb.onrender.com/'
 };
 
 const outputFile = './swagger-output.json';
